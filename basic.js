96abf1183713dbe06da97688f8f5b055024e3841
Your Name <you@example.com>
2024-11-22 10:18:26 +0200

    kalkulatoram neiet + un - tikai * un :  :(

---
+++ basic.js
@@ -0,0 +1,19 @@
+let personAge = 17
+console.log(personAge >= 18)
+
+namesArray = {
+    1: "Jānis",
+    2: "Anna",
+    3: "Zaiga",
+}
+
+for (let i = 1; i <= 3; i++) {
+    console.log(namesArray[i])
+  }
+
+
+function sayHello(name) {
+    console.log("Labdien,", name,"!")
+}
+
+sayHello("Markuss");
